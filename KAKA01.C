7da0f346cb6d11e9b3dd76a026f80f967215f5e3
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-29 11:38:21 +0530

    Add files via upload

---
+++ KAKA01.C
@@ -0,0 +1,17 @@
+// WAP to sum of numbers?
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1, num2, num3, sum;
+	
+	printf("Enter the value of a : \n");
+	scanf("%d", &num1);
+	printf("Enter the value of b : \n");
+	scanf("%d", &num2);
+	printf("Enter the value of c : \n");
+	scanf("%d", &num3);
+	sum = num1+num2+num3;
+	printf("The result of d is = %d \n", sum);
+	getch();
+}
