3cc2b7ebca4970c1a77e53b9c34798d9931944f8
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-30 19:29:13 +0530

    Update C3.c

--- C3.c
+++ C3.c
@@ -1,17 +1,19 @@
 // WAP to subtract of numbers?
 #include<stdio.h>
-#include<conio.h>
+
 void main()
 {
-	int a, v, c, d;
-	clrscr();
-	printf("Enter the value of a : \n");
-	scanf("%d", &a);
-	printf("Enter the value of v : \n");
-	scanf("%d", &v);
-	printf("Enter the value of c : \n");
-	scanf("%d", &c);
-	d = a-v-c;
-	printf("The result of d is = %d \n");
-	getch();
+	int num1, num2, num3, sub;
+	
+	printf("Enter the value of number 1 : \n");
+	scanf("%d", &num1);
+	printf("Enter the value of number 2 : \n");
+	scanf("%d", &num2);
+	printf("Enter the value of number 3 : \n");
+	scanf("%d", &num3);
+	
+	sub = num1 - num2 - num3;
+	printf("Subtract of three number is = %d \n", sub);
+
+	return 0;
 }
