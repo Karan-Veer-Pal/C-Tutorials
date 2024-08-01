f28da23238122654a8919006818e6b8672a5f7e4
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-01 11:03:45 +0530

    Update C5.c

--- C5.c
+++ C5.c
@@ -1,17 +1,19 @@
 // WAP to divide of numbers?
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {
-	int a, v, c, d;
-	clrscr();
-	printf("Enter the value of a : \n");
-	scanf("%d", &a);
-	printf("Enter the value of v : \n");
-	scanf("%d", &v);
-	printf("Enter the value of c : \n");
-	scanf("%d", &c);
-	d = a/v/c;
-	printf("The result of d is = %d \n");
-	getch();
+	int num1, num2. num3. div;
+	
+	printf("Enter the value of number 1 : \n");
+	scanf("%d", &num1);
+	printf("Enter the value of number 2 : \n");
+	scanf("%d", &num2);
+	printf("Enter the value of number 3 : \n");
+	scanf("%d", &num3);
+	
+	div = num1 / num2 / num3;
+	printf("Divide of three number is = %d \n", div);
+
+	return 0;
 }
