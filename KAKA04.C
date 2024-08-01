ff5523a4962dcd95ff907e1e0f21e6568e3e5bac
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-01 11:01:03 +0530

    Add files via upload

---
+++ KAKA04.C
@@ -0,0 +1,17 @@
+// WAP to divide of numbers?
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a, v, c, d;
+	clrscr();
+	printf("Enter the value of a : \n");
+	scanf("%d", &a);
+	printf("Enter the value of v : \n");
+	scanf("%d", &v);
+	printf("Enter the value of c : \n");
+	scanf("%d", &c);
+	d = a/v/c;
+	printf("The result of d is = %d \n");
+	getch();
+}
