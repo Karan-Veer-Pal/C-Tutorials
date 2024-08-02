efe06d331517c80aa4ec2a2bb7eeabc7db13fcc2
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-02 12:15:54 +0530

    Update C06.c

--- C06.c
+++ C06.c
@@ -6,7 +6,7 @@ int main()
 {
 	int a = 7.47;
 	
-	printf(" Example of implicit type convertion : %d \n", a);
+	printf("Example of implicit type convertion : It automatically convert the float (7.47) into integer: %d \n", a);
 
 	return 0;
 }
