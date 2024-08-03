1a0d019c15220eecee99adf51e0034494f0fb11c
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-03 11:00:34 +0530

    Update C07.c

--- C07.c
+++ C07.c
@@ -1,11 +1,13 @@
-// WAP to declare explicit type conversion?
+// Day : 07 WAP to declare explicit type conversion?
+
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {
 	int a = 5, b;
-	clrscr();
+	
 	b = (float)a;
 	printf(" Example of explicit type convertion : %f \n", (float)b);
-	getch();
+
+	return 0;
 }
