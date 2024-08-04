c51f1ead983ab791d8494196f6a64ef049e63d17
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-04 16:23:38 +0530

    Update C08.c

--- C08.c
+++ C08.c
@@ -1,13 +1,15 @@
-// WAP to declare type conversion?
+// Day : 08 WAP to demontrate type conversion?
+
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {
 	float a = 4.5, b = 5.5, c;
-	clrscr();
+	
 	c = a+b;
 	printf(" The value of c is %f \n ", c);
 	c = (int)a+b;
 	printf(" The value of c is %d \n ", (int)c);
-	getch();
+
+	return 0;
 }
