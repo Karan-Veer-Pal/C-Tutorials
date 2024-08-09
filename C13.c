8aa38f61952f1851a41ecc0ff3075ca17561226c
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-09 16:43:29 +0530

    Update C13.c

--- C13.c
+++ C13.c
@@ -1,20 +1,20 @@
-// WAP to check the student are eligible or not?
+// Day : 13 WAP to check the student are eligible or not to fill the sarkari form?
+
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {       int student;
-	clrscr();
+	
 	printf("Enter the age of student:");
 	scanf("%d", &student);
 	if(student>=18)
 	{
 		printf("The student is eligible to fill the form");
-		// These student are adult.
 	}
 	else
 	{
 		printf("The student is not eligible to fill the form");
-		// These student are not adult.
 	}
-	getch();
+
+ 	return 0;
 }
