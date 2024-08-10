1fa3236715618aa785cf8efdddf0015d7f914846
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-10 15:36:51 +0530

    Add files via upload

---
+++ KAKA13.C
@@ -0,0 +1,28 @@
+// WAP to calculate the electricity bill?
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float unit, bill;
+	clrscr();
+	printf("Enter the value of unit:");
+	scanf("%f", &unit);
+	if(unit<=100)
+	{
+		bill = 4.5*unit;
+	}
+	else if(unit>100 && unit<=200)
+	{
+		bill = 5*unit;
+	}
+	else if(unit>200 && unit<=300)
+	{
+		bill = 5.5*unit;
+	}
+	else
+	{
+		bill = 6*unit;
+	}
+	printf("The bill is = %f", bill);
+	getch();
+}
