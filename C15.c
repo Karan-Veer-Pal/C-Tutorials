ca36e0df87ab6c3513606c8e3d161674c212f4bc
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-11 14:00:11 +0530

    Update C15.c

--- C15.c
+++ C15.c
@@ -1,14 +1,16 @@
-// WAP to calculate a salary
+// Day : 15 WAP to calculate a salary?
+
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {
 	float da, bs, gs;
-	clrscr();
+	
 	printf("Enter the value of bs:");
 	scanf("%f", &bs);
 	da = bs*32/100;
 	gs = bs+da;
 	printf("The gs is = %f", gs);
-	getch();
+	
+	return 0;
 }
