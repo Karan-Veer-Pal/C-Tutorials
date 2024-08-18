c2b170ddcfc93427d7c6f18b63d89c5f54bf0545
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-18 17:07:18 +0530

    Add files via upload

---
+++ KAKA21.C
@@ -0,0 +1,16 @@
+// WAP to perform a for loop?
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+	clrscr();
+       //	printf("Enter any number: \n");
+       //	scanf("%d");
+	for(i=1; i<10; i++)
+	{
+		printf("%d \n", i);
+	}
+	getch();
+}
