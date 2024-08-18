55c07f4cb800c3e6d02d27168085be3fb9b6dd62
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-18 17:08:52 +0530

    Update C22.c

--- C22.c
+++ C22.c
@@ -1,17 +1,17 @@
 // Day : 22 WAP to perform a for loop?
 
 #include<stdio.h>
-#include<conio.h>
 
-void main()
+int main()
 {
 	int i;
-	clrscr();
-       //	printf("Enter any number: \n");
-       //	scanf("%d");
+
+       //printf("Enter any number: \n");
+       //scanf("%d");
 	for(i=1; i<10; i++)
 	{
 		printf("%d \n", i);
 	}
-	getch();
+
+	return 0;
 }
