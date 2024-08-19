3e6fd35d207d674d1922a50225a91d1a9400de3a
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-19 18:38:33 +0530

    Update C23.c

--- C23.c
+++ C23.c
@@ -1,16 +1,17 @@
-// WAP to display integers number from 1 to n?
+// Day : 23 WAP to display integers number from 1 to n?
+
 #include<stdio.h>
-#include<conio.h>
 
 void main()
 {
 	int n, i;
-	clrscr();
+	
 	printf("Enter the value of n:");
 	scanf("%d", &n);
 	for(i=0; i<=n; i++)
 	{
 		printf("%d \n", i);
 	}
-	getch();
+
+	return 0;
 }
