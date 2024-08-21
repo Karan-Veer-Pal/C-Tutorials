12f905698e851f278586761603256f71ee32acd4
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-21 15:08:46 +0530

    Update C25.c

--- C25.c
+++ C25.c
@@ -1,11 +1,11 @@
-// WAP to find even number by using for loop with if statement?
-#include<stdio.h>
-#include<conio.h>
+// Day : 25 WAP to find even number by using for loop with if statement?
 
-void main()
+#include<stdio.h>
+
+int main()
 {
 	int i, n;
-	clrscr();
+	
 	printf("Enter the value of n is : \n");
 	scanf("%d", &n);
 	for(i=0; i<=n; i++)
@@ -15,5 +15,6 @@ void main()
 			printf("%d \n", i);
 		}
 	}
-	getch();
+
+	return 0;
 }
