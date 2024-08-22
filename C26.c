53290b38a1ae45dfa6f89827a5aaa75b40f567e5
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-22 18:39:02 +0530

    Update C26.c

--- C26.c
+++ C26.c
@@ -1,16 +1,17 @@
-// WAP to print a table of given number?
-#include<stdio.h>
-#include<conio.h>
+// Day : 26 WAP to print a table of given number?
 
-void main()
+#include<stdio.h>
+
+int main()
 {
 	int i, n;
-	clrscr();
-	printf("Enter the value of n is:");
+	
+	printf("Enter the value of n is :\n ");
 	scanf("%d", &n);
 	for(i=1; i<=10; i++)
 	{
 		printf("%d \n", i*n);
 	}
-	getch();
+
+	return 0;
 }
