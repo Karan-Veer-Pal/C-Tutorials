2c9ee391f1851375bb5876e83822863d1032bac0
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-24 11:34:59 +0530

    Update C28.c

--- C28.c
+++ C28.c
@@ -1,11 +1,11 @@
-// WAP using break statement?
-#include<stdio.h>
-#include<conio.h>
+// Day : 28 WAP using break statement?
 
-void main()
+#include<stdio.h>
+
+int main()
 {
 	int i, n;
-	clrscr();
+	
 	//printf("Enter the value of n is:");
 	//scanf("%d", &n);
 	for(i=1; i<=10; i++)
@@ -16,6 +16,6 @@ void main()
 		}
 		printf("%d \n", i);
 	}
-	getch();
 
+	return 0;
 }
