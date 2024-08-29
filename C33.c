17fc5f83540ce1c4738126868c99325a6c21471a
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-29 12:22:07 +0530

    Update C33.c

--- C33.c
+++ C33.c
@@ -1,22 +1,24 @@
-// WAP to demonstrate of function with argument with return a value?
+// Day : 33 WAP to demonstrate of function with argument with return a value?
 
 #include<stdio.h>
-#include<conio.h>
 
 int Sum(int x, int y);
-void main()
+int main()
 {
 	int a, b, c;
-	clrscr();
+	
 	printf("Enter the value of a and b:");
 	scanf("%d %d", &a, &b);
+	
 	c = Sum(a,b);
 	printf("The sum is = %d", c);
-	getch();
+	
+	return 0;
 }
 int Sum(int x, int y)
 {
 	int z;
 	z = x+y;
+	
 	return z;
 }
