f0332ed63bac1637c5dee22a6f212c18aa56694c
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-30 12:50:04 +0530

    Update C34.c

--- C34.c
+++ C34.c
@@ -1,19 +1,19 @@
-// WAP to demonstrate the concept of call by value?
+// Day : 34 WAP to demonstrate the concept of call by value?
 
 #include<stdio.h>
-#include<conio.h>
 
 void Add_10(int x, int y);
-void main()
+int main()
 {
 	int a, b;
-	clrscr();
+	
 	printf("\n Enter the value of a and b:");
 	scanf("%d %d", &a, &b);
 	printf("\n Before function value od a = %d and b = %d", a, b);
 	Add_10(a,b);
 	printf("\n After function value of a = %d and b = %d", a, b);
-	getch();
+
+	return 0;
 }
 void Add_10(int x, int y)
 {
