41eaf8a9fa1de176d431bab8f12947902692ec71
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-01 13:42:17 +0530

    Add files via upload

---
+++ KAKA35.C
@@ -0,0 +1,18 @@
+// WAP of unconditional statement go to?
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+	printf("hello");
+	goto Hii;
+	printf("good morning");
+	printf("good afternoon");
+	printf("good evening");
+	Hii;
+	printf("good night");
+	getch();
+
+}
