cb44bcfaedc8a06b6a2615e0b3dc2e91cac4741a
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-02 11:45:57 +0530

    Add files via upload

---
+++ KAKA36.C
@@ -0,0 +1,12 @@
+// WAP to show the use of register storage class?
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	register int a;
+	clrscr();
+	printf("The value of a = %d", a);
+	getch();
+}
