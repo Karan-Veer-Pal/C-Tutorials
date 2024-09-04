7d060fc6e95a4c9a68b8add89c555821b827add4
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-04 12:06:27 +0530

    Update C39.c

--- C39.c
+++ C39.c
@@ -1,12 +1,12 @@
-// WAP to show the use of auto storage class?
+// Day : 39 WAP to show the use of auto storage class?
 
 #include<stdio.h>
-#include<conio.h>
 
-void main()
+int main()
 {
 	auto int a;
-	clrscr();
+	
 	printf("The value of a = %d", a);
-	getch();
+
+	return 0;
 }
