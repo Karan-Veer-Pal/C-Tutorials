a685f04ff83a266b78b686d7468b2e9ef21f4614
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-12 11:31:04 +0530

    Add files via upload

---
+++ KAKA46.C
@@ -0,0 +1,17 @@
+// WAP to input a string and display them?
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char st[50];
+	clrscr();
+
+	printf("Enter the string:\n");
+	scanf("%s", st);
+
+	printf("The string is %s\n", st);
+
+	getch();
+}
