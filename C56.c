68e3fa5c33ad2495a206c57201375518f1e207eb
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-23 11:57:16 +0530

    Update C56.c

--- C56.c
+++ C56.c
@@ -1,16 +1,15 @@
 // Day : 56 WAP pointer with array?
-#include<stdio.h>
-#include<conio.h>
 
-void main()
+#include<stdio.h>
+
+int main()
 {
 	int a[5] = {3,2,9,5};
 	int *ptr;
 	ptr = a;
-	clrscr();
 
 	printf("Element of the array are:\n");
 	printf("%d %d %d %d", ptr[0], ptr[1], ptr[2], ptr[3]);
 
-	getch();
+	return 0;
 }
