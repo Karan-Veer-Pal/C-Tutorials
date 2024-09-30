62303c8cbbfa6ce8d51ce8c6a28aa18be4298a05
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-30 14:16:23 +0530

    Update C63.c

--- C63.c
+++ C63.c
@@ -1,15 +1,13 @@
-// WAP using a function which calculate the sum and average of three numbers.
+// Day : 63 WAP using a function which calculate the sum and average of three numbers.
 
 #include<stdio.h>
-#include<conio.h>
 
 int sum(int x, int y, int z);
 float average(float avg);
-void main()
+int main()
 {
 	int a, b, c, w;
 	float av;
-	clrscr();
 
 	printf("Enter the value of a, b and c:\n");
 	scanf("%d%d%d", &a, &b, &c);
@@ -20,7 +18,7 @@ void main()
 	av = average(w);
 	printf("The value of average is:%f\n", av);
 
-	getch();
+	return 0;
 }
 int sum(int x, int y, int z)
 {
