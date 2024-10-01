816284a215a08d295ddb199ce7c160b118dc7d31
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-01 11:52:52 +0530

    Update C64.c

--- C64.c
+++ C64.c
@@ -1,13 +1,12 @@
-// WAP using a function which calculate the sum and average of two numbers. Use pointers and print the value of sum  and average in main()?
+// Day : 64 WAP using a function which calculate the sum and average of two numbers. Use pointers and print the value of sum and average in main()?
+
 #include<stdio.h>
-#include<conio.h>
 
 void SumandAvg(int x, int y, int *sum, float *avg);
-void main()
+int main()
 {
 	int a, b, sum;
 	float avg;
-	clrscr();
 
 	printf("Enter the value of a and b:\n");
 	scanf("%d%d", &a, &b);
@@ -16,7 +15,7 @@ void main()
 	printf("The value of sum is:%d\n", sum);
 	printf("The value of average is:%f\n", avg);
 
-	getch();
+	return 0;
 }
 void SumandAvg(int x, int y, int *sum, float *avg)
 {
