d0ae18c60d2b428221da05e4814c921e7515e705
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-06 13:05:15 +0530

    Update C67.c

--- C67.c
+++ C67.c
@@ -1,23 +1,30 @@
 // Day : 67 WAP to writing some conteny or text to a file?
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	int n;
-	FILE *fp;
+int main() {
+    int n;
+    FILE *fp;
 
-	fp = fopen("demo.txt","r+");
-	if(fp == NULL)
-	{
-		printf("Error!");
-		exit(1);
-	}
-	scanf(fp,"%d", &n);
+    // Open the file in write mode
+    fp = fopen("demo.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening the file!");
+        exit(1);
+    }
 
+    // Read input from the user
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-	printf("The value of n=%d",n);
+    // Write the input to the file
+    fprintf(fp, "The value of n = %d\n", n);
 
-	return 0;
+    // Close the file
+    fclose(fp);
+
+    printf("The value of n = %d has been written to demo.txt\n", n);
+
+    return 0;
 }
