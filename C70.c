948782b7215d418b286644711f16846fc990467e
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-07 13:14:33 +0530

    Update C70.c

--- C70.c
+++ C70.c
@@ -1,20 +1,22 @@
-//WAP to demonstrate a right shift and left shift?
-#include<stdio.h>
-#include<conio.h>
+// Day : 70 WAP to demonstrate a right shift and left shift?
 
-void main()
+#include<stdio.h>
+
+int main()
 {
 	int a = 212, i;
-	clrscr();
 
 	for(i=0; i<=2; i++)
 	{
 		printf("Right shift by %d:%d\n",i,a>>i);
 	}
+	
 	printf("\n");
+	
 	for(i=0; i<=2; i++)
 	{
 		printf("Left shift by %d:%d\n", i,a<<i);
 	}
-	getch();
+	
+	return 0;
 }
