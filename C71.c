705547fb233000f242be8b0d5487ca291f794d7f
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-08 16:39:13 +0530

    Update C71.c

--- C71.c
+++ C71.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define l 100
 #define b 50
 
@@ -5,7 +7,7 @@ int main()
 {
 	int area;
 
-	area = l+b;
+	area = l*b;
 	printf("The area of the rectangle is:%d", area);
 
 	return 0;
