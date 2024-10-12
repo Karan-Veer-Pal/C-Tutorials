e786049e9885ed5bc14fa794377d2f1c82cf61b8
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-12 19:55:48 +0530

    Update C75.c

--- C75.c
+++ C75.c
@@ -1,13 +1,11 @@
-//WAP to accept marks of five students in an array and print them on a screen?
+// Day : 75 WAP to accept marks of five students in an array and print them on a screen?
+
 #include<stdio.h>
-#include<conio.h>
 
-
-void main()
+int main()
 {
 	int n, i;
 	int arr[100];
-	clrscr();
 
 	printf("Enter how many student in a class:\n");
 	scanf("%d", &n);
@@ -22,5 +20,6 @@ void main()
 		printf("The marks of student %d:\n", i+1);
 		printf("%d\n", arr[i]);
 	}
-	getch();
+
+	return 0;
 }
