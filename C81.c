50a6850db15e8f73d5738ed131a53c6f4ab13deb
Karan Veer Pal <karanpalveer@gmail.com>
2024-10-23 18:36:59 +0530

    Update and rename KAKA80.C to C81.c

--- C81.c
+++ C81.c
@@ -1,13 +1,12 @@
-// WAP to access the array by using function?
+// day : 81 WAP to access the array by using function?
+
 #include<stdio.h>
-#include<conio.h>
 
 void arrayfunction(int *ptr, int n);//ptr stores the address of array
-void main()                         //n stores the size of array
+int main()                         //n stores the size of array
 {
 	int arr[100];//={57,89,3,5,23,65,87,90};
 	int size, i;
-	clrscr();
 
 	printf("Enter the size of the array:\n");
 	scanf("%d", &size);
@@ -20,7 +19,8 @@ void main()                         //n stores the size of array
 	arrayfunction(arr,size);
 	printf("The value of arr[3] is %d\n", arr[3]);//It will change the-
 	//actual array coz we using call by reference we will give address.
-	getch();
+	
+	return 0;
 }
 void arrayfunction(int *ptr, int n)
 {
