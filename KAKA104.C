73d27b1059c3fb0e393920028ce4f3880f99ad46
Karan Veer Pal <karanpalveer@gmail.com>
2024-11-16 19:49:56 +0530

    Add files via upload

---
+++ KAKA104.C
@@ -0,0 +1,25 @@
+//WAP to copy the string to another without using library function?
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char str1[32], str2[32];
+	int i;
+	clrscr();
+
+	printf("Enter the string is:\n");
+	gets(str1);
+	printf("The string is:\n");
+	puts(str1);
+
+	for(i=0; str1[i]!='\0'; i++)
+	{
+		str2[i]=str1[i];
+	}
+	str2[i]!='\0';
+	printf("The copied string is:\n");
+	puts(str2);
+
+	getch();
+}
