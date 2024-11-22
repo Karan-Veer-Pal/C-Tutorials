5a8402679a84845e97291f9c956778c973b01654
Karan Veer Pal <karanpalveer@gmail.com>
2024-11-22 12:46:06 +0530

    Update C110.c

--- C110.c
+++ C110.c
@@ -1,15 +1,13 @@
-//WAP to access the address of the variable?
+// Day : 110 WAP to access the address of the variable?
+
 #include<stdio.h>
-#include<conio.h>
 
 void main()
 {
 	int *ptr, a;
 	a=&ptr;
-	clrscr();
 
 	printf("The address of the variable a is:%u\n", a);
 
-
-	getch();
+	return 0;
 }
