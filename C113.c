d985e3e810639b1a3bfe3db972ac5443dd26d8e9
Karan Veer Pal <karanpalveer@gmail.com>
2024-11-25 10:22:13 +0530

    Update C113.c

--- C113.c
+++ C113.c
@@ -1,16 +1,16 @@
-//Write a function slice() to slice a string. It should change the original string such that it is now the sliced string. Take m and n as the start and ending position for slice?
+// Day : 113 Write a function slice() to slice a string. It should change the original string such that it is now the sliced string. Take m and n as the start and ending position for slice?
+
 #include<stdio.h>
-#include<conio.h>
 
 void slice(char *str, int m, int n);
-void main()
+int main()
 {
 	char st[]="systumhai";
-	clrscr();
+
 	slice(st,2,12);
 	printf("%s", st);
 
-	getch();
+	return 0;
 }
 void slice(char *str, int m, int n)
 {
