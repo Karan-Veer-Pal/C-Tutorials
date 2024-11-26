0f11466195e62fde4fa5b283c7dda408d074a5ea
Karan Veer Pal <karanpalveer@gmail.com>
2024-11-26 17:43:18 +0530

    Add files via upload

---
+++ KAKA113.C
@@ -0,0 +1,40 @@
+//WAP to encrypt and decrypt a string by adding 1 to the ASCII value of its characters?
+#include<stdio.h>
+#include<conio.h>
+
+void encrypt(char *c);
+void decrypt(char *a);
+void main()
+{
+	char str1[100];
+	clrscr();
+
+	printf("Enter any string or a message which you want to be encrypt or decrypt:\n");
+	gets(str1);
+
+	encrypt(str1);
+	printf("Encrypt string or message is:%s\n", str1);
+	decrypt(str1);
+	printf("Decrypt string or message is:%s\n", str1);
+
+
+	getch();
+}
+void encrypt(char *c)
+{
+	char *ptr=c;
+	while(*ptr!='\0')
+	{
+		*ptr=*ptr+1;
+		ptr++;
+	}
+}
+void decrypt(char *a)
+{
+	char *p=a;
+	while(*p!='\0')
+	{
+		*p=*p-1;
+		p++;
+	}
+}
